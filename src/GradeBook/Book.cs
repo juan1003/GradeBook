b00307e1e5aeecb831a079c5e345619f98129029
Juan De Jesus <jdejesus93@gmail.com>
2021-08-05 14:43:01 -0400

    Book class refactor

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
-  class Book
+  public class Book
   {
     public Book(string name)
     {
@@ -15,21 +15,31 @@ namespace GradeBook
       grades.Add(grade);
     }
 
+    public Statistics GetStatistics()
+    {
+
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low= double.MaxValue;
+
+            foreach(var grade in grades)
+            {
+                result.Low = Math.Min(grade, result.Low);
+                result.High = Math.Max(grade, result.High);
+                result.Average += grade;
+            }
+            result.Average /= grades.Count;
+
+            return result;
+    }
+
     public void ShowStatisics()
     {
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowestGrade = double.MaxValue;
-            foreach(var number in grades)
-            {
-                lowestGrade = Math.Min(number, lowestGrade);
-                highGrade = Math.Max(number, highGrade);
-                result += number;
-            }
-            result /= grades.Count;
-            System.Console.WriteLine($"The Lowest Grade is {lowestGrade}");
-            System.Console.WriteLine($"The highst grade is {highGrade}");
-            Console.WriteLine($"The average grade is {result:N1}");
+      var stats = this.GetStatistics();
+      System.Console.WriteLine($"The average grade is {stats.Average}");
+      System.Console.WriteLine($"The lowest grade is {stats.Low}");
+      System.Console.WriteLine($"The highest grade is {stats.High}");
     }
 
     private List<double> grades;
