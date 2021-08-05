c2892169e292918690dc56d49fa873cd18314711
Juan De Jesus <jdejesus93@gmail.com>
2021-08-05 13:52:30 -0400

    Create Book.cs

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+  class Book
+  {
+    public Book(string name)
+    {
+      grades = new List<double>();
+      this.name = name;  
+    }
+    public void AddGrade(double grade)
+    {
+      grades.Add(grade);
+    }
+
+    public void ShowStatisics()
+    {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowestGrade = double.MaxValue;
+            foreach(var number in grades)
+            {
+                lowestGrade = Math.Min(number, lowestGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
+            }
+            result /= grades.Count;
+            System.Console.WriteLine($"The Lowest Grade is {lowestGrade}");
+            System.Console.WriteLine($"The highst grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");
+    }
+
+    private List<double> grades;
+    private string name;
+  }
+}
