b4b6f8615fba62562c79b6898f825c41d55587b5
Juan De Jesus <jdejesus93@gmail.com>
2022-06-30 15:31:03 -0500

    Finished Course

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -10,6 +10,31 @@ namespace GradeBook
       grades = new List<double>();
       Name = name;  
     }
+
+    public void AddLetterGrade(char letter)
+    {
+      switch(letter)
+      {
+        case 'A':
+          AddGrade(90);
+          break;
+        case 'B':
+          AddGrade(80);
+          break;
+        case 'C':
+          AddGrade(70);
+          break;
+        case 'D':
+          AddGrade(60);
+          break;
+        case 'F':
+          AddGrade(50);
+          break;
+        default:
+          AddGrade(0);
+          break;
+      }
+    }
     public void AddGrade(double grade)
     {
       grades.Add(grade);
@@ -31,6 +56,25 @@ namespace GradeBook
             }
             result.Average /= grades.Count;
 
+            switch(result.Average)
+            {
+              case var d when d >= 90.0:
+                result.Letter = 'A';
+                break;
+              case var d when d >= 80.0:
+                result.Letter = 'B';
+                break;
+              case var d when d >= 70.0:
+                result.Letter = 'C';
+                break;
+              case var d when d >= 60.0:
+                result.Letter = 'D';
+                break;
+              default:
+                result.Letter = 'F';
+                break;
+            }
+
             return result;
     }
 
@@ -40,9 +84,16 @@ namespace GradeBook
       System.Console.WriteLine($"The average grade is {stats.Average:N1}");
       System.Console.WriteLine($"The lowest grade is {stats.Low}");
       System.Console.WriteLine($"The highest grade is {stats.High}");
+      System.Console.WriteLine($"The letter grade is {stats.Letter}");
     }
 
     private List<double> grades;
-    public string Name;
+    public string Name
+    {
+      get;
+      private set;
+    }
+
+    readonly string category = "Science";
   }
 }
